b9a08d8517f117e7af78a77fc93644f6e31b1245
Ramkumar Kollimalayan <kraamkumar@outlook.com>
2022-06-17 21:39:21 +0530

    commit

--- html/js/script.js
+++ html/js/script.js
@@ -1,3 +1,84 @@
 $(document).ready(function(){
+    AOS.init();
 
+    var $navbar = $(".header_nav .navbar");
+    adjustheader();
+
+    $(window).scroll(function() {
+        adjustheader();
+    });
+
+    function adjustheader(){
+        if ($(window).scrollTop() > 138) {
+            if (!$navbar.hasClass("sticky-nav")) {
+                $navbar.addClass("sticky-nav");
+            }
+        } else {
+            $navbar.removeClass("sticky-nav");
+        }
+    }
+
+    $("#project").click(function () {
+        var navHeight = $(".header_nav .navbar").outerHeight();
+        $("html, body").animate({
+            scrollTop: $(".section-four").offset().top - navHeight
+        },500);
+    });
+
+    $("#city").click(function () {
+        var navHeight = $(".header_nav .navbar").outerHeight();
+        $("html, body").animate({
+            scrollTop: $(".section-three").offset().top - navHeight
+        },500);
+    });
+    
+    $("#developer").click(function () {
+        var navHeight = $(".header_nav .navbar").outerHeight();
+        $("html, body").animate({
+            scrollTop: $(".section-two").offset().top - navHeight
+        },500);
+    });
+    /*
+    $("#project").click(function () {
+        var navHeight = $(".header_nav .navbar").outerHeight();
+        $("html, body").animate({
+            scrollTop: $(".section-two").offset().top - navHeight
+        },500);
+    });
+
+    $("#city").click(function () {
+        var navHeight = $(".header_nav .navbar").outerHeight();
+        $("html, body").animate({
+            scrollTop: $(".section-two").offset().top - navHeight
+        },500);
+    });
+
+    $("#developer").click(function () {
+        var navHeight = $(".header_nav .navbar").outerHeight();
+        $("html, body").animate({
+            scrollTop: $(".section-two").offset().top - navHeight
+        },500);
+    });
+
+    $("#project").click(function () {
+        var navHeight = $(".header_nav .navbar").outerHeight();
+        $("html, body").animate({
+            scrollTop: $(".section-two").offset().top - navHeight
+        },500);
+    });
+
+    $("#city").click(function () {
+        var navHeight = $(".header_nav .navbar").outerHeight();
+        $("html, body").animate({
+            scrollTop: $(".section-two").offset().top - navHeight
+        },500);
+    });
+
+    $("#developer").click(function () {
+        var navHeight = $(".header_nav .navbar").outerHeight();
+        $("html, body").animate({
+            scrollTop: $(".section-two").offset().top - navHeight
+        },500);
+    });
+    */
 });
